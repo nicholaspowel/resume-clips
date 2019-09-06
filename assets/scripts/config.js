2d4bbb44ec358d30a2c936a31d04c22f4fecdf19
nicholaspowel <nicholaspowel@gmail.com>
2019-09-06 11:07:18 -0400

    Connect heroku app to config file

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://resume-clips-rails-api.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
