c701af94598a7f1cf947b24218e0d4a39f9c4248
nicholaspowel <nicholaspowel@gmail.com>
2019-09-08 15:08:09 -0400

    Parse Data from clips form
    Successfully parsing the data from the save-clips form field

--- assets/scripts/clips/events.js
+++ assets/scripts/clips/events.js
@@ -1,13 +1,16 @@
 'use strict'
 const api = require('./api.js')
 const ui = require('./ui.js')
+const getFormFields = require('./../../../lib/get-form-fields.js')
 
 const onSaveBook = (event) => {
-
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  console.log('The new clip data is:', data, event)
 }
 
 const addHandlers = () => {
-  $('.content').on('click', '.save-btn', onSaveBook)
+  $('.content').on('submit', '#save-clip', onSaveBook)
 }
 
 module.exports = {
