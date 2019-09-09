1e7978e1f7476266d3df161498d5269eef36f6b8
nicholaspowel <nicholaspowel@gmail.com>
2019-09-09 17:33:56 -0400

    Copy alerts.js and messages.js from Jen

---
+++ assets/scripts/common/messages.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const messages = {
+  signUpFailure: 'Oh my! Something went wrong.  Try again.',
+  signUpSuccess: 'Sweet! You\'re sign up. Now sign in.',
+  signInFailure: 'Booooooo... Something went wrong.  Try again.',
+  signInSuccess: 'You\'re signed in. Have fun!',
+  signOutFailure: 'Oh my! Something went wrong.  Try again.',
+  signOutSuccess: 'Sorry to see you go... Come back soon!',
+  changePasswordFailure: 'Something went wrong and your password was not changed.  Try again.',
+  changePasswordSuccess: 'Done! Password changed.'
+}
+
+module.exports = messages
