a2520775da78610eef166e121d80f06777ffcb02
nicholaspowel <nicholaspowel@gmail.com>
2019-09-11 11:48:45 -0400

    Fix bug with update fields
    Fixed a bug in the update form fields that couldn't handle null data

--- assets/scripts/templates/helpers/hasDate.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const hasDate = (date) => {
-  return date !== null ? date : ''
-}
-
-module.exports = hasDate
---
+++ assets/scripts/templates/helpers/hasVal.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const hasVal = (val) => {
+  return val !== null ? val : ''
+}
+
+module.exports = hasVal
